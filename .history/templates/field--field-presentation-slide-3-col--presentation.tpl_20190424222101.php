c487bca626c2236e915e1ff02f8c49bb0cf19e5b
chekka <info@chekka.de>
2019-10-20 00:57:44 +0200

    update

---
+++ .history/templates/field--field-presentation-slide-3-col--presentation.tpl_20190424222101.php
@@ -0,0 +1,7 @@
+<h1><?php print $title; ?></h1>
+<?php endif; ?>
+<div class="field-items"<?php print $content_attributes; ?>>
+<?php foreach ($items as $delta => $item): ?>
+ <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+<?php endforeach; ?>
+</div>
